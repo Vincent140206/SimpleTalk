cc6a2b8b6692847c47fb00c0e58edb5a7b252bec
Vincent140206 <vincentiusdylan01@gmail.com>
2025-06-30 13:36:08 +0700

    add socket io logic

---
+++ lib/core/services/socket_services.dart
@@ -0,0 +1,32 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketService {
+  late IO.Socket socket;
+
+  void connect() {
+    socket = IO.io(
+      'http://10.0.2.2:5000',
+      IO.OptionBuilder()
+        .setTransports(['websocket'])
+        .disableAutoConnect()
+        .build(),
+    );
+
+    socket.connect();
+
+    socket.onConnect((_) => print('Connected to server'));
+    socket.onDisconnect((_) => print('Disconnected from server'));
+  }
+
+  void sendMessage(String message) {
+    socket.emit('sendMessage', {'message': message});
+  }
+
+  void onMessage(Function(dynamic) callback) {
+    socket.on('receiveMessage', callback);
+  }
+
+  void dispose() {
+    socket.dispose();
+  }
+}
